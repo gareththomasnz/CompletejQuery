cb39750e61408912bdd44675b6409de371290e5f
gareththomasnz <gareththomasnz@yahoo.com>
2016-08-02 16:57:42 +1200

    third  commit

--- js/script.js
+++ js/script.js
@@ -10,25 +10,50 @@ $(function() {
    //$(".blue-box").fadeToggle(1000).slideUp(1000);
    //$("p").hide().fadeIn(1000);
    
-   $(".blue-box").animate({
-        "margin-left": "200px",
-        "opacity": "0",
-        "height": "50px",
-        "width": "50px",
-        "margin-top": "25px"
-   },1000);
+   //$(".blue-box").animate({
+   //     "margin-left": "200px",
+   //     "opacity": "0",
+   //     "height": "50px",
+   //     "width": "50px",
+   //     "margin-top": "25px"
+   //},1000);
+   //
+   //$(".red-box").animate({
+   //     "width": "120px",
+   //     "height": "120px"
+   //},1000);   
+   //
+   //$("p").animate({
+   //     "font-size": "24px"
+   //     },1000);
+   //
+   //$("h1").animate({
+   //     "font-size": "54px"
+   //     },1000);
    
-   $(".red-box").animate({
-        "width": "120px",
-        "height": "120px"
-   },1000);   
-   
-   $("p").animate({
-        "font-size": "24px"
-        },1000);
-   
-   $("h1").animate({
-        "font-size": "54px"
-        },1000);
-   
-});
+        //$(".red-box").fadeTo(1000, 0.2).delay(1000);
+        //$(".green-box").fadeTo(1000, 0.5).delay(2000);  
+        //$(".blue-box").fadeTo(1000, 0.8).fadeOut(500).delay(500).fadeIn(500);
+       //$(".blue-box").animate({
+       // "margin-left": "200px"
+       // });
+       //$(".blue-box").animate({
+       // "margin-left": "0px"
+       // });
+
+       //callback functions
+       
+      //$(".red-box").fadeTo(1000, 0, function() {
+      //        $(".green-box").fadeTo(1000, 0, function() {
+      //                $(".blue-box").fadeTo(1000, 0);
+      //                });
+      //        });
+      
+      //$(".blue-box").hide(1000);
+      //$(".blue-box").show(1000);
+      
+
+        $(".lightbox").delay(400).fadeIn(1000);
+
+
+      });
