7811df7062f9a2ab35cde0c23d8a4f0be0cb9262
gareththomasnz <gareththomasnz@yahoo.com>
2016-08-02 13:40:44 +1200

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# jQuery Exercises
+
+Complete jquery 
+
+## Built With
+
+Komodo - hand coding exercise
+
+
+## Authors
+
+Gareth Thomas
+
+## License
+
+This is coded from a commercial course - not public domain
+
+## Acknowledgments
+
+Udemy course
---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(function() {
+
+  // effects fade & slide
+   //$(".red-box").fadeTo(1000, 0.1);
+   //$(".red-box").slideUp(2000);
+   //$(".red-box").slideDown(2000);
+   //$(".green-box").fadeTo(1000, 0.5).slideUp(5000);
+   //$(".blue-box").fadeToggle(1000);
+   //$(".blue-box").fadeToggle(1000).slideUp(1000);
+   
+   
+   $("p").hidden().fadeIn(1000);
+});
