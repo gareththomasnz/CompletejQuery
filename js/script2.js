7f764efe40f73d2446699cc183853f47fa44d64b
gareththomasnz <gareththomasnz@yahoo.com>
2016-08-04 15:20:16 +1200

    seventh  commit

--- js/script2.js
+++ js/script2.js
@@ -10,10 +10,53 @@ $(function() {
         //var checkbox = $("input:checkbox");
         //console.log(checkbox.prop("checked"));
         
-        var inputText = $("input:text");
-        console.log(inputText.val());
+        //var inputText = $("input:text");
+        //console.log(inputText.val());
+        //
+        //var rangeValue = $("input[type='range']");
+        //console.log(rangeValue.val());
         
-        var rangeValue = $("input[type='range']");
-        console.log(rangeValue.val());
+        //$("li").remove();
+        //$("form").children().not("input:text, textarea, br").remove();
+        
+        //var detachedListItem = $("li").detach();
+        //$("content").append(detachedListItem);
+        
+        //$("p:first").empty();
+
+        //$(".red-box, .green-box, .blue-box").empty();
+        
+        //$("li").replaceWith("<li> replaced with");
+        
+        //$("li").replaceWith(function(){
+        //        return "<li> replaced with function </li>";
+        //        });
+        
+        //var firstListItem = $("li:first");
+        //$("p").replaceWith(firstListItem);
+        
+        //$(".red-box, .blue-box").replaceWith("<div class='green-box'> More Green </div>");
+        //$("<div class='green-box'> More Green </div>").replaceAll(".red-box, .blue-box");
+        
+        //$(".gallery").css("display", "none");
+        //
+        //var redBox = $(".red-box");
+        //console.log(redBox.css("width"));
+        //console.log(redBox.width());
+        //
+        //redBox.css("background-color", "#AA0077");
+        //$("p").css("font-size", "18px");
+        //redBox.css("width", "+=20px");
+        //
+        //var properties = $("p").css(["font-size", "line-height", "color"]);
+        //console.log(properties);
+        
+        //redBox.css("user-select", "none");
+        
+        //redBox.css("user-select", function(){
+        //        return none;
+        //        });
+        
+        
         
       });
