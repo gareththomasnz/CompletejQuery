a3f92d64681820c4b8f9ed8053ce42a61d5bf12c
gareththomasnz <gareththomasnz@yahoo.com>
2016-08-06 11:43:13 +1200

    twelth  commit

--- js/script4.js
+++ js/script4.js
@@ -18,13 +18,26 @@ $(function() {
         //        }
         //        });         
         
-        $("#checkbox").change(function(){
-                var isChecked = $(this).is(":checked");
-                if(isChecked){
-                        $(this).add("label[for='cb']").css("box-shadow", "0 0 4px #818");
-                }else{
-                        $(this).css("box-shadow", "0 0 4px #181");
-                }
-                });
+        //$("#checkbox").change(function(){
+        //        var isChecked = $(this).is(":checked");
+        //        if(isChecked){
+        //                $(this).add("label[for='cb']").css("box-shadow", "0 0 4px #818");
+        //        }else{
+        //                $(this).css("box-shadow", "0 0 4px #181");
+        //        }
+        //        });
+        //
+        //$("#selection").change(function(){
+        //        var selectedOption = $(this).find(":selected").text();
+        //        alert(selectedOption);
+        //        });
+        
+        //$("#form").submit(function(event){
+        //        var textArea = $("#message");
+        //        if(textArea.val().trim() === ""){
+        //                textArea.css("box-shadow", "0 0 4px #811");
+        //                event.preventDefault();
+        //        }else{}
+        //        });
         
       });
